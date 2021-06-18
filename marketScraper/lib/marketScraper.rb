9ef9aa8a980496dc4f88cc25cb91e950306f8b78
Joseph DeGregorio <degregorio.joseph@gmail.com>
2021-06-18 15:22:06 +0000

    Automatically backed up by Learn

--- marketScraper/lib/marketScraper.rb
+++
@@ -1,6 +0,0 @@
-require "marketScraper/version"
-
-module MarketScraper
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- marketScraper/lib/marketScraper/version.rb
+++
@@ -1,3 +0,0 @@
-module MarketScraper
-  VERSION = "0.1.0"
-end
